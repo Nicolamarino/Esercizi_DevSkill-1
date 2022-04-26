88ad7278667b9899075a33e16f23dfb803539a57
Nicola Marino <mar.nicola98@gmail.com>
2022-04-26 17:24:35 +0200

    converter funzionante

--- Convertitore/src/app/home/home.page.ts
+++ Convertitore/src/app/home/home.page.ts
@@ -5,8 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
+  c:number;
+  f:number;
+  km:number;
+  mi:number;
 
   constructor() {}
+
+  converter( unita:string):void{
+    if ( unita ==="c"){
+      this.f = this.arrotonda((this.c * 9/5) + 32);
+    }else if (unita === "f"){
+      this.c =  this.arrotonda((this.f - 32) * 5 / 9); 
+    }else if (unita ==="km"){
+      this.mi = this.arrotonda(this.km / 1.609);
+    }else{
+      this.km = this.arrotonda(this.mi * 1.609);
+    }
+  }
+  arrotonda(risultato:number):number{
+    return +risultato.toPrecision(3);
+   }
 
 }
