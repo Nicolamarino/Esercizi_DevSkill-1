5d91c509c11d66a4fd0c864a6b0e9ef16aea0736
Nicola Marino <mar.nicola98@gmail.com>
2022-05-17 17:24:16 +0200

    spostati file nelle cartelle

--- anno nascita.ts
+++
@@ -1,6 +0,0 @@
-function eta(annoNascita:number){
-    return "Hai " + (2022 - annoNascita) + " anni."
-}
-let annoNascita:number = prompt("Inserisci il tuo anno di nascita") *1;
-
-console.log(eta(annoNascita));
